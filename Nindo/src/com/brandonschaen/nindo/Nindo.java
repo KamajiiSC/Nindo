31fb529cf12ff61b69c8cda293652bc2168c15ca
KamajiiSC <brandonlschaen@gmail.com>
2021-01-28 18:33:34 -0500

    Changed Title, changed text to sceneTitle, and created css file to style and positioned sceneTitle at top

--- Nindo/src/com/brandonschaen/nindo/Nindo.java
+++ Nindo/src/com/brandonschaen/nindo/Nindo.java
@@ -1,8 +1,10 @@
 package com.brandonschaen.nindo;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Nindo extends Application{
@@ -13,10 +15,17 @@ public class Nindo extends Application{
 	
 	@Override
 	public void start( Stage primaryStage ) {
-		primaryStage.setTitle("Hello World");
 		StackPane root = new StackPane();
-		root.getChildren();
-		primaryStage.setScene(new Scene(root, 1300, 900));
+		Scene scene = new Scene(root, 1300, 900);
+		Text sceneTitle = new Text();
+		
+		scene.getStylesheets().add("MainScene.css");
+		sceneTitle.setText("Nindo");
+		sceneTitle.getStyleClass().add("header-title");
+		primaryStage.setTitle("Nindo");
+		root.getChildren().add(sceneTitle);
+		root.setAlignment(Pos.TOP_CENTER);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
