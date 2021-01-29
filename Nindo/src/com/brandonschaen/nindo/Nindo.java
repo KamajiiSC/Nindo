12be422a709c8256ef1ad53428e3d20f15870195
KamajiiSC <brandonlschaen@gmail.com>
2021-01-28 21:44:12 -0500

    Set options background to white, selected background to gray, and auto select 0

--- Nindo/src/com/brandonschaen/nindo/Nindo.java
+++ Nindo/src/com/brandonschaen/nindo/Nindo.java
@@ -3,8 +3,9 @@ package com.brandonschaen.nindo;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 public class Nindo extends Application{
@@ -15,16 +16,26 @@ public class Nindo extends Application{
 	
 	@Override
 	public void start( Stage primaryStage ) {
-		StackPane root = new StackPane();
+		BorderPane root = new BorderPane();
 		Scene scene = new Scene(root, 1300, 900);
 		Text sceneTitle = new Text();
+		ListView<String> options = new ListView<String>();
 		
+		options.getItems().add(" New Game ");
+		options.getItems().add(" Load Game ");
+		options.getItems().add(" Options ");
+		options.getItems().add(" Exit ");
 		scene.getStylesheets().add("MainScene.css");
 		sceneTitle.setText("Nindo");
 		sceneTitle.getStyleClass().add("header-title");
+		options.getStyleClass().add("option-list");
 		primaryStage.setTitle("Nindo");
-		root.getChildren().add(sceneTitle);
-		root.setAlignment(Pos.TOP_CENTER);
+		
+		root.setTop(sceneTitle);
+		root.setCenter(options);
+		BorderPane.setAlignment(sceneTitle, Pos.CENTER);
+		options.getSelectionModel().select(0);
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
